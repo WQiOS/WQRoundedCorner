5aedc3f5b6ddffcff671b99450c7ed4b04be6c81
王强 <1570375769@qq.com>
2018-03-20 13:21:32 +0800

    修改文件

--- WQRoundedCorner/UIButton+WQRadius.h
+++ WQRoundedCorner/UIButton+WQRadius.h
@@ -26,14 +26,6 @@
                      size:(CGSize)size
                  forState:(UIControlState)state;
 
-/**设置 contentMode 模式的圆角背景图*/
-- (void)setCornerWQRadius:(WQRadius)radius
-                   imageURL:(NSURL *)imageURL
-                placeholder:(UIImage *)placeholder
-                contentMode:(UIViewContentMode)contentMode
-                       size:(CGSize)size
-                    orState:(UIControlState)state;
-
 /**配置所有属性配置圆角背景图*/
 - (void)setCornerWQRadius:(WQRadius)radius
                    imageURL:(NSURL *)imageURL
